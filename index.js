7d0e77e2d2b52f9006d3463933feef7e4da69363
Safi ahmed <Safi ahmed>
2017-06-15 15:24:00 +0500

    Token name change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let token = ""
 // Facebook 
 
 app.get('/webhook/', function(req, res) {
-	if (req.query['hub.verify_token'] === "blondiebytes") {
+	if (req.query['hub.verify_token'] === "Sajeel") {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("Wrong token")
